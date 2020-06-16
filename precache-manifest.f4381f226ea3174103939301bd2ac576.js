02d137a11945f0cf0816c9eb170c72b8f8ff906d
Ron Huberfeld <32178925+ron-huberfeld@users.noreply.github.com>
2020-06-16 16:43:19 +0300

    Updates

---
+++ precache-manifest.f4381f226ea3174103939301bd2ac576.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733c1171e08e4fee623b512712e7f1d9",
+    "url": "/Match-EU-FrontEnd/index.html"
+  },
+  {
+    "revision": "43dec91160d877269bef",
+    "url": "/Match-EU-FrontEnd/static/css/2.760d6bf1.chunk.css"
+  },
+  {
+    "revision": "3f63ad08f9083b68df0d",
+    "url": "/Match-EU-FrontEnd/static/css/main.d9f1547f.chunk.css"
+  },
+  {
+    "revision": "43dec91160d877269bef",
+    "url": "/Match-EU-FrontEnd/static/js/2.1873cefc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Match-EU-FrontEnd/static/js/2.1873cefc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f63ad08f9083b68df0d",
+    "url": "/Match-EU-FrontEnd/static/js/main.fa3ffddb.chunk.js"
+  },
+  {
+    "revision": "a7ee1fb84360183854eb",
+    "url": "/Match-EU-FrontEnd/static/js/runtime-main.e806c9cd.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Match-EU-FrontEnd/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Match-EU-FrontEnd/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Match-EU-FrontEnd/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Match-EU-FrontEnd/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Match-EU-FrontEnd/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/Match-EU-FrontEnd/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/Match-EU-FrontEnd/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/Match-EU-FrontEnd/static/media/revicons.2feb69cc.eot"
+  }
+]);
