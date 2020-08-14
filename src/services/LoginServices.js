57b3889941952e8a38c561e6f19c000ad7f45638
Ron Huberfeld <32178925+ron-huberfeld@users.noreply.github.com>
2020-08-15 00:54:24 +0300

    Add API_URL environment variable

--- src/services/LoginServices.js
+++ src/services/LoginServices.js
@@ -1,4 +1,4 @@
-import http from '../http-common';
+import http from '../utils/http-common';
 
 const login = (email, password) => {
     return http.post('/login', {
--- src/utils/http-common.js
+++ src/utils/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://restful-matcheu.herokuapp.com/',
+    baseURL: process.env.REACT_APP_API_URL,
     headers: {
         'Content-type': 'application/json',
     },
